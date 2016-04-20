b5e0feeb5aad7cfad6ed9664a5a77504158458d3
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-04-20 17:55:04 +0200

    Outlets for description view created.

---
+++ iHIIT WatchKit Extension/DescriptionInterfaceController.swift
@@ -0,0 +1,33 @@
+//
+//  DescriptionInterfaceController.swift
+//  iHIIT
+//
+//  Created by Carlos Rodríguez Domínguez on 20/4/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class DescriptionInterfaceController: WKInterfaceController {
+    @IBOutlet var descriptionLabel: WKInterfaceLabel!
+    @IBOutlet var movie: WKInterfaceMovie!
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
