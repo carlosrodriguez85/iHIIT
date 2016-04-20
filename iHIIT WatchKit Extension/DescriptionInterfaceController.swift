39dd1bc1af334093a1b42e335bd5a0d81358a2c6
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-04-21 00:25:06 +0200

    Make use of multithreading to make the description interface controller to instantly appear.

--- iHIIT WatchKit Extension/DescriptionInterfaceController.swift
+++ iHIIT WatchKit Extension/DescriptionInterfaceController.swift
@@ -37,16 +37,22 @@ class DescriptionInterfaceController: WKInterfaceController {
             self.setTitle(exercise.name)
             self.descriptionLabel.setText(exercise.description)
             
-            HCYoutubeParser.thumbnailForYoutubeURL(exercise.youtubeURL, thumbnailSize: YouTubeThumbnailDefaultMedium, completeBlock: { (image, error) in
-                if error == nil {
-                    self.videoThumbnail.setImage(image)
-                }
-                else{
-                    self.videoThumbnail.setImage(UIImage(named: "hiit_background"))
-                }
+            self.videoButton.setHidden(true)
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
+                self.videoURL = exercise.videoURL
+                
+                HCYoutubeParser.thumbnailForYoutubeURL(exercise.youtubeURL, thumbnailSize: YouTubeThumbnailDefaultMedium, completeBlock: { (image, error) in
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if error == nil {
+                            self.videoThumbnail.setImage(image)
+                        }
+                        else{
+                            self.videoThumbnail.setImage(UIImage(named: "hiit_background"))
+                        }
+                        self.videoButton.setHidden(false)
+                    })
+                })
             })
-            
-            self.videoURL = exercise.videoURL
         }
     }
 
