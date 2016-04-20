8c401e5dc1492807c51a8878bb745970e9fceb3c
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-04-20 13:58:10 +0200

    Initial Commit

---
+++ iHIIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iHIIT
+//
+//  Created by Carlos Rodríguez Domínguez on 20/4/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
