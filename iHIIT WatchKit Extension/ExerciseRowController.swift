c89f686107e6d90be7b8359bd675103e427783e1
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-04-20 17:39:50 +0200

    ExerciseRowController added. Exercises table added. Table row controller set to ExerciseRowController.

---
+++ iHIIT WatchKit Extension/ExerciseRowController.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseRowController.swift
+//  iHIIT
+//
+//  Created by Carlos Rodríguez Domínguez on 20/4/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+import Foundation
+
+class ExerciseRowController : NSObject {
+    
+}
