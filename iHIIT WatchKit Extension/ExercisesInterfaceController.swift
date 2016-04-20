2e645a4c2120f16f2c3c49233bd1c3ef607b70f1
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-04-20 17:46:59 +0200

    ExercisesInterfaceController class created and linked to storyboard corresponding interface controller. Table outlet created.

---
+++ iHIIT WatchKit Extension/ExercisesInterfaceController.swift
@@ -0,0 +1,33 @@
+//
+//  ExercisesInterfaceController.swift
+//  iHIIT
+//
+//  Created by Carlos Rodríguez Domínguez on 20/4/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ExercisesInterfaceController: WKInterfaceController {
+    @IBOutlet var table: WKInterfaceTable!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
